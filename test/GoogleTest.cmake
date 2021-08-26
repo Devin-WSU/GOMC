07bfda376c81b95de169f0d59080415d27315671
GregorySchwing <gschwing@uno.edu>
2021-08-26 16:33:44 -0500

    Example uses ensemble specific example

--- test/GoogleTest.cmake
+++ test/GoogleTest.cmake
@@ -1,4 +1,6 @@
-function(add_mpi_test name no_mpi_proc)
+#EnsemblePreprocessor defines NVT = 1, GEMC = 2, GCMC = 3, NPT = 4
+
+function(add_NVT_mpi_test name no_mpi_proc)
   set(NVT_flags "-DENSEMBLE=1")
       # My test are all called name_test.cpp
       add_executable(${name} ${GOMCMPISources} ${GOMCMPIHeaders} ${libHeaders} ${libSources} ${MPITestSources})
@@ -6,6 +8,20 @@ function(add_mpi_test name no_mpi_proc)
   target_link_libraries(${name} ${MPI_LIBRARIES} gtest_main)
   set_target_properties(${name} PROPERTIES 
       COMPILE_FLAGS "${NVT_flags}")
+  #set(test_parameters " ${MPIEXEC_NUMPROC_FLAG} ${no_mpi_proc} ./${name}")
+  #add_test(NAME ${name} COMMAND "${MPIEXEC} ${test_parameters}")
+  add_test(NAME ${name} COMMAND ParallelTemperingTest)
+endfunction(add_NVT_mpi_test)
+
+function(add_NPT_mpi_test name no_mpi_proc)
+  #NPT (Isothermal-Isobaric) Ensemble
+  set(NPT_flags "-DENSEMBLE=4")
+      # My test are all called name_test.cpp
+      add_executable(${name} ${GOMCMPISources} ${GOMCMPIHeaders} ${libHeaders} ${libSources} ${MPITestSources})
+  # Make sure to link MPI here too:
+  target_link_libraries(${name} ${MPI_LIBRARIES} gtest_main)
+  set_target_properties(${name} PROPERTIES 
+      COMPILE_FLAGS "${NPT_flags}")
   #set(test_parameters " ${MPIEXEC_NUMPROC_FLAG} ${no_mpi_proc} ./${name}")
   #add_test(NAME ${name} COMMAND "${MPIEXEC} ${test_parameters}")
   add_test(NAME ${name} COMMAND ParallelTemperingTest)
@@ -47,7 +63,8 @@ endif()
 include(test/FileList.cmake)
 
 if(GOMC_GTEST_MPI)
-  add_mpi_test(GOMC_MPI_Test 2)
+  add_NVT_mpi_test(GOMC_NVT_MPI_Test 2)
+  add_NPT_mpi_test(GOMC_NPT_MPI_Test 2)
   set(GOMC_GTEST_MPI 1)
 endif()
 
